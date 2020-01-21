6d7d3d5f6523b6ea03164243bc895a52c70b69ff
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2020-01-21 17:56:53 -0500

    Added comments.

--- conditional_actions.py
+++ conditional_actions.py
@@ -1,12 +1,18 @@
+# Get a number from a user
 number = int(input("Enter a number:"))
+
+# If number is odd, print Weird
 if (number % 2 != 0):
     print("Weird")
 
+# If number is even and in the inclusive range of 2 to 5, print Not Weird
 elif ((number % 2 == 0) and (number in  range(2, 5))):
     print("Not Weird")
 
+# If number is even and in the inclusive range of 6 to 20, print weird
 elif ((number % 2 == 0) and (number in range (6, 20))):
     print("Weird")
 
+# If number is even and greater than 20, print Not Weird
 elif ((number % 2 == 0) and (number > 20)):
     print("Not Weird")
